3898c16b6ed78a61254ca7d367139070bfd42cdd
mpuertob <marcospuertobarrero1@gmail.com>
2021-01-14 23:39:47 +0100

    venga queda poco

--- src/app/horario/horario.page.ts
+++ src/app/horario/horario.page.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from "@angular/core";
 import { ActivatedRoute, Router } from "@angular/router";
-import { Asignatura } from "../core/model/asignatura";
 import { Horario } from "../core/model/horario";
 import { DatosMockService } from "../share/datos-mock.service";
 
@@ -31,8 +30,6 @@ export class HorarioPage implements OnInit {
     return this.horario.obtenerAsignatura(hora);
   }
   obtenerNombreCompleto(abreviatura) {
-    alert(
-      "NombreCompleto: " + this.datosMock.obtenerNombreCompleto(abreviatura)
-    );
+    alert(this.datosMock.obtenerNombreCompleto(abreviatura));
   }
 }
